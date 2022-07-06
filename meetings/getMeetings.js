aebe3611e11a04c2d23299cd835123e32836cd69
andres857 <andres857@gmail.com>
2022-07-06 11:28:15 -0500

    create meetings

--- meetings/getMeetings.js
+++ meetings/getMeetings.js
@@ -1,28 +1,25 @@
 require('dotenv').config()
 
 const bbb = require('bigbluebutton-js')
-const crypto = require('crypto')
 const api = bbb.api(process.env.BBB_URL, process.env.BBB_SECRET)
 const http = bbb.http
 
-let createUrlOne = api.administration.create("meeting-testing", crypto.randomUUID(), {
-  duration: 5,
-  attendeePW: 'secret',
-  moderatorPW: 'supersecret',
-})
 
-let infosUrl = api.monitoring.getMeetings()
-
-let run = async () => {
-  await http(createUrlOne)
-  let result = await http((infosUrl))
-  let salasActivas = result.meetings.filter(running => running.running === true);
-
-  console.log(`hay ${salasActivas.length} meeting activos`);
-  console.log(result);
-  // console.log(salasActivas[1].attendees);
-  console.log('----------------------------*****-------------------------');
+let getMeetings = async () => {
+  let meetings = await http(api.monitoring.getMeetings())
+  console.log(meetings.meetings);
+  moderatorUrl = api.administration.join('moderator', '3e390a4f-1fce-4e2a-b571-23f343024514', 'dontsecreet')
   console.log(moderatorUrl);
 }
 
-run()
+getMeetings()
+
+
+
+  // let salasActivas = result.meetings.filter(running => running.running === true);
+
+  // console.log(`hay ${salasActivas.length} meeting activos`);
+  // console.log(result);
+  // // console.log(salasActivas[1].attendees);
+  // console.log('----------------------------*****-------------------------');
+  // console.log(moderatorUrl);
