3d74ac5df8b94291296cda9d2703f00567ceb962
andres857 <andres857@gmail.com>
2023-02-13 16:27:30 -0500

    se agrega los estilos para el fondo de la reunion

--- meetings/createMeetings.js
+++ meetings/createMeetings.js
@@ -16,14 +16,14 @@ let meetingCreateUrl = api.administration.create('testing rooms 10', meetingID, 
   // duration: 1000,
   attendeePW: passAttendeePW,
   moderatorPW: passModeratorPW,
-  logoutURL: "https://myzonego.com",
+  logoutURL: "https://myzonego.com/live",
   welcome: "Welcome to your BigBlueButton meeting!",
   allowStartStopRecording: "true",
   autoStartRecording: "false",
-  record: "false",
+  record: "true",
   lockSettingsDisablePublicChat: "true",
-  bannerColor: "#FAE69E", 
-  bannerText: "Saludos coordiales",
+  // bannerColor: "#FAE69E", 
+  // bannerText: "Saludos coordiales",
   muteOnStart: "true",
   allowModsToUnmuteUsers: "true",
   allowModsToEjectCameras: "true",
@@ -42,23 +42,71 @@ const meetingCreate = async() =>{
      console.log('Error creando el meeting');
   }
   
-  moderatorUrl = api.administration.join('adminandres', url_meeting.meetingID, passModeratorPW)
-  attendeeUrl = api.administration.join('invitenandres', url_meeting.meetingID, passAttendeePW, {
+  const kwParamsjoin = {
     "userdata-bbb_skip_check_audio": "true",
     "userdata-bbb_show_public_chat_on_login": "false",
     // "userdata-bbb_hide_nav_bar": "true"
-    "userdata-bbb_show_participants_on_login": "false",
-    "userdata-bbb_hide_actions_bar": "true",
-    "userdata-bbb_custom_style": ".hmnuRG{background-color:red !important;}"
-  })
-  console.log(`Moderator link: ${moderatorUrl}\nAttendee link: ${attendeeUrl}`)
+    "userdata-bbb_show_participants_on_login": "true",
+    "userdata-bbb_hide_actions_bar": "false",
+
+    "userdata-bbb_custom_style": `
+    .sc-hZyDwR {
+      background-image: url(https://www.myzonego.com/storage/tenant-8/W39xM43mUgiAJ6lpnqQg18onXkKizJiP49nwDeFD.jpg);
+      background-size: cover; 
+      border-left-width: 1px;
+      border-color: grey;
+      border-left-style: solid;
+    }
+    .bodyVw{
+      background-color: transparent ;
+    }
+    .irODRp{
+      background-color: transparent ;
+    }
+    /* recording-description */
+    .jsEDqp { 
+      background-color: #0d4d9a ;
+      border-color: #227C9D ;
+      padding: 7px 13px 7px 9px;
+      box-shadow: 0 .125rem .25rem rgba(0,0,0,.075);
+    }
+    .jsEDqp span {
+      font-weight: bold;
+    }
+    .dcDKgv {
+      font-weight: 600;
+      color: #221212;
+    }
+    .cwAETT > span{
+      box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
+      background-color: #cfd2cde3 !important;
+      border-width: 2px;
+    }
+    .jeAcue > span {
+      background-color: #EE0056
+    }
+    .jeAcue:hover > span {
+      background-color: #EE0056 
+    }
+    .jkgjts:focus, .buttonWrapper:focus[data-test="leaveVideo"].jkgjts{
+      background-color: green
+    }`
+  };
+
+  moderatorUrl = api.administration.join('adminandres', url_meeting.meetingID, passModeratorPW,kwParamsjoin)
+  // attendeeUrl = api.administration.join('invitenandres', url_meeting.meetingID, passAttendeePW,kwParamsjoin )
+  // console.log(`Moderator link: ${moderatorUrl}\nAttendee link: ${attendeeUrl}`)
+  console.log(`Moderator link: ${moderatorUrl}`)
+
 }
 
 meetingCreate()
 module.exports = meetingCreate
 
 
-
+// .jkgjts{
+    //   background-color: #EE0056 !important
+    // }
 
 
 
